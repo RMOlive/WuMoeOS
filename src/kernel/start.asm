35f661b346ae20a8e41788e8356d5977169d9379
RMOlive <rmolives@gmail.com>
2022-06-11 13:02:44 +0800

    commit

--- src/kernel/start.asm
+++ src/kernel/start.asm
@@ -9,10 +9,10 @@ extern kernel_init
 
 global _start
 _start:
-	push ebx
-	push eax
-	call console_init
-	call gdt_init
-	call memory_init
-	call kernel_init
-	jmp $
+    push ebx
+    push eax
+    call console_init
+    call gdt_init
+    call memory_init
+    call kernel_init
+    jmp $
